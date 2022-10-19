6c68eb7fadc260b992cc3beb4d51a3a2181fcd43
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-19 18:05:25 +0300

    components UI 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/register' exact element={<Register />} />
+          <Route path='/:id' exact element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
