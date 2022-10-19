a63bb255beda7191c8e476df2ff39be7187b26b4
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-19 18:53:15 +0300

    models setup

---
+++ server/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    dislikes: {
+      type: [String],
+      default: [],
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Post', PostSchema);
---
+++ server/models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: '',
+    },
+    coverPicture: {
+      type: String,
+      default: '',
+    },
+    followers: {
+      type: [String],
+    },
+    followings: {
+      type: [String],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: String,
+      enum: ['single', 'relationship', 'complicated'],
+      default: 'single',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
