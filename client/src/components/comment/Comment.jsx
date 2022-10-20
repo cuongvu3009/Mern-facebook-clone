704617dee8b38bfc9dad1aeda06d10371b3cf70e
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-20 22:15:50 +0300

    setup comment box

---
+++ client/src/components/comment/Comment.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './comment.css';
+import { AiOutlineSend } from 'react-icons/ai';
+
+const Comment = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className='comment-wrapper'>
+      <div className='comment-bar'>
+        <img
+          src={currentUser.profilePicture}
+          alt=''
+          className='postProfileImg'
+        />
+        <input type='text' id='comment-box' placeholder='comment...' />
+        <button className='comment-btn' type='submit'>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
