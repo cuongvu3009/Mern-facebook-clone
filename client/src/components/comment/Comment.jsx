9aaaf38933d120a9b594ab1da93707eabbd5a99f
cuongvu3009 <cuongvu3009@gmail.com>
2022-10-20 22:16:29 +0300

    setup comment box

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './comment.css';
-import { AiOutlineSend } from 'react-icons/ai';
 
 const Comment = () => {
   const { currentUser } = useSelector((state) => state.user);
